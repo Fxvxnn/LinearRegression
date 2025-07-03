7bcbfc210639aab66c919b926bd41dc06e411291
Fynn Aßmann <76216167+Fxvxnn@users.noreply.github.com>
2025-07-03 10:28:50 +0200

    Add files via upload

---
+++ linearRegression.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+double operator*(const std::vector<double>& a, const std::vector<double> b) {
+    if (a.size() != b.size()) {
+        throw std::invalid_argument("Vectors must be the same size for dot product.");
+    }
+
+    double out {0.0};
+    for (size_t i = 0; i < a.size(); i++) {
+        out += a[i] * b[i];
+    }
+    return out;
+}
+
+
+class linearRegression
+{
+private:
+    std::vector<double> theta;
+    double lr;
+public:
+    linearRegression(int feature_size, double _lr) : lr(_lr) {
+        theta.resize(feature_size);
+    }
+    double predict(std::vector<double>& x) {
+        return theta * x;
+    }
+    void update(std::vector<double>& x, double y) {
+        for (size_t i=0; i<theta.size(); i++) {
+            theta[i] += lr * (y-predict(x))*x[i];
+        }
+    }
+    void SGD(std::vector<std::vector<double>>& features, std::vector<double>& targets) {
+        for (size_t i=0; i<features.size(); i++) {
+            update(features[i], targets[i]);
+        }
+    }
+    void BGD(std::vector<std::vector<double>>& features, std::vector<double>& targets) {
+        for (size_t j=0; j<theta.size(); j++) {
+            double sum {0};
+            for (size_t i=0; i<features.size(); i++) {
+                sum += (targets[i]-predict(features[i])) * features[i][j];
+            }
+            theta[j] += lr * sum;
+        }
+    }
+    double mse(std::vector<std::vector<double>>& features, std::vector<double>& targets) {
+        double out {0};
+        for (size_t i=0; i<features.size(); i++) {
+            out += (targets[i]-predict(features[i])) * (targets[i]-predict(features[i]));
+        }
+        out = out / features.size();
+        return out;
+    }
+};
+
+void read_csv(std::vector<std::vector<double>>& features, std::vector<double>& targets, std::string path) {
+    std::ifstream file(path);
+
+    std::string line;
+    std::getline(file, line);
+    while (std::getline(file, line)) {
+        std::stringstream ss(line);
+        std::string cell;
+        std::vector<double> x {1};
+
+        for (int i=0; i<2; i++) {
+            std::getline(ss, cell, ',');
+            x.push_back(std::stod(cell));
+        }
+        for (int i=0; i<1; i++) {
+            std::getline(ss, cell, ',');
+            x.push_back((cell=="Yes"));
+        }
+        for (int i=0; i<2; i++) {
+            std::getline(ss, cell, ',');
+            x.push_back(std::stod(cell));
+        }
+        features.push_back(x);
+
+        std::getline(ss, cell, ',');
+        targets.push_back(std::stod(cell));
+    }
+    file.close();
+
+}
+
+int main() {
+
+    double TEST_SPLIT {0.2};
+
+    std::vector<std::vector<double>> features;
+    std::vector<double> targets;
+
+    read_csv(features, targets, "Student_Performance.csv");
+    
+    std::vector<std::vector<double>> test_features(features.end()-TEST_SPLIT*features.size(), features.end());
+    std::vector<double> test_targets(targets.end()-TEST_SPLIT*targets.size(), targets.end());
+
+    features.resize((1-TEST_SPLIT)*features.size());
+    targets.resize((1-TEST_SPLIT)*targets.size());
+
+
+    linearRegression model(features[0].size(), 1e-4);
+
+    for (int i=0; i<=500; i++) {
+        model.SGD(features, targets);
+        std::cout << model.mse(test_features, test_targets) << "  -----------  Iteration: " << i << std::endl;
+    }
+
+    std::cout << model.predict(test_features[100]) << "  -----------  " << test_targets[100] << std::endl;
+
+    std::cout << model.mse(test_features, test_targets) << std::endl;
+    return 0;
+}
